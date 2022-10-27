b8d5cac02fe7d1a2ca984718b4faba8f217a93c2
Rafael Alberto Larios <albertolarios@me.com>
2022-10-27 10:39:53 -0500

    Add class to filled or partially filled input nodes

--- script.js
+++ script.js
@@ -2,20 +2,41 @@ console.log("Hello world!")
 
 const passwdA = document.getElementById("passwd");
 const passwdB = document.getElementById("cpasswd");
+const inputForms = Array.from(document.querySelectorAll(".ff-common"));
 
-[passwdA, passwdB].map(x => x.addEventListener("input", setValidity))
+inputForms.map(x => x.addEventListener("input", setFilled));
+
+[passwdA, passwdB].map(x => x.addEventListener("input", setValidity));
 
 function setValidity() {
     if (!anyPasswdsEmpty() && !arePasswdsEqual()) {
         this.setCustomValidity("Passwords must match.");
         this.reportValidity();
-        [passwdA, passwdB].map(x => x.className = "error")
+        [passwdA, passwdB].map(x => {
+            x.className = "error";
+            x.classList.remove("no-error");});
+    }
+    else if (!anyPasswdsEmpty() && arePasswdsEqual()) {
+        [passwdA, passwdB].map(x => x.className = "no-error")
     }
     else {
         [passwdA, passwdB].map(x => {
         x.setCustomValidity("");
-        x.classList.remove("error");})
+        x.classList.remove("error");
+        x.classList.remove("no-error");})
     }
+}
+
+function setFilled() {
+    if (!isInputEmpty(this)) {
+        this.className = "not-empty";
+    }
+    else { this.classList.remove("not-empty"); }
+}
+
+function isInputEmpty(inputNode) {
+    console.log("You are here")
+    return inputNode.value === "";
 }
 
 function arePasswdsEqual() {
